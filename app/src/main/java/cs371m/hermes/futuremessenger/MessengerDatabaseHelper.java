864ad90706d93e70316da97d8306ebf9228e635c
Samarth Dhruva <samarthdhruva@utexas.edu>
2016-07-15 23:45:15 -0500

    Added more information to the basic database helper class, introduced the idea of an association table between messages and recipients.

--- app/src/main/java/cs371m/hermes/futuremessenger/MessengerDatabaseHelper.java
+++ app/src/main/java/cs371m/hermes/futuremessenger/MessengerDatabaseHelper.java
@@ -14,14 +14,25 @@ public class MessengerDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "futuremessenger.db";
 
+
+    //Names of columns in the Recipient table in the database.
+    public static final String RECIPIENT_TABLE_NAME = "recipient_table";
+    public static final String RECIPIENT_ID = "ID";
+    public static final String RECIPIENT_PHONE_NUMBER = "PHONE_NUMBER";
+    public static final String RECIPIENT_NAME = "NAME";
+
     // Names of various columns in the Message Table in the database.
     public static final String MESSAGE_TABLE_NAME = "message_table";
     public static final String MESSAGE_ID = "ID";
-    public static final String MESSAGE_RECIPIENTS = "RECIPIENTS";
-    public static final String MESSAGE_TIME = "TIME";
-    public static final String MESSAGE_DATE = "DATE";
+    public static final String MESSAGE_DATETIME = "DATETIME";
     public static final String MESSAGE_TXT_CONTENT = "TEXT_CONTENT";
     public static final String MESSAGE_IMG_PATH = "IMAGE_PATH";
+
+    // Names of various columns in the Recipient-Message association
+    // table in the database.
+    public static final String REC_MESS_TABLE_NAME = "recipient_message_table";
+    public static final String RECEP_ID = "RECIPIENT_ID";
+    public static final String MESS_ID = "MESSAGE_ID";
 
     // Names of various columns in the Preset table in the database.
     public static final String PRESET_TABLE_NAME = "preset_table";
@@ -34,6 +45,7 @@ public class MessengerDatabaseHelper extends SQLiteOpenHelper {
     public MessengerDatabaseHelper(Context context, String name,
                                    SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -49,26 +61,40 @@ public class MessengerDatabaseHelper extends SQLiteOpenHelper {
            SHOULD BE FIXED LATER
 
            */
+
+        // Create the Recipients table that will hold our recipients.
+        db.execSQL("create table " + RECIPIENT_TABLE_NAME + "(" +
+                    RECIPIENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    RECIPIENT_PHONE_NUMBER + " TEXT," +
+                    RECIPIENT_NAME + " TEXT)");
+
         // Create the Message table that will hold our messages.
         db.execSQL("create table " + MESSAGE_TABLE_NAME + "(" +
                 MESSAGE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                MESSAGE_TIME + " TEXT," +
-                MESSAGE_DATE + " TEXT," +
-                MESSAGE_RECIPIENTS + " TEXT," +
+                MESSAGE_DATETIME + " TEXT," +
                 MESSAGE_TXT_CONTENT + " TEXT," +
                 MESSAGE_IMG_PATH + " TEXT)");
 
-        // Create the Preset table that will hold our presets.
+        // Create the Recipients_Messages table that will hold associations
+        // between Messages and Recipients. (A message can have many recipients
+        // and a recipient could have many associated messages.)
+        db.execSQL("create table " + REC_MESS_TABLE_NAME + "(" +
+                    RECEP_ID + " TEXT," +
+                    MESS_ID + " TEXT," +
+                    "PRIMARY KEY(" + RECEP_ID + ", " + MESS_ID + ")");
+
+/*        // Create the Preset table that will hold our presets.
         db.execSQL("create table " + PRESET_TABLE_NAME + "(" +
                 PRESET_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 PRESET_NAME + " TEXT," +
-                PRESET_CONTENT + " TEXT)");
+                PRESET_CONTENT + " TEXT)");*/
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + RECIPIENT_TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + MESSAGE_TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + PRESET_TABLE_NAME);
+        //db.execSQL("DROP TABLE IF EXISTS " + PRESET_TABLE_NAME);
         onCreate(db);
     }
 }

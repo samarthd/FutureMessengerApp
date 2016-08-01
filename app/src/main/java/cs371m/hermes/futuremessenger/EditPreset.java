acf9c523964b9554a7fa591c522d985f967c82f8
Billy Do <dob@utexas.edu>
2016-07-31 22:11:21 -0500

    ManagePreset can call EditPreset, EditPreset has code ready for editing presets

--- app/src/main/java/cs371m/hermes/futuremessenger/EditPreset.java
+++ app/src/main/java/cs371m/hermes/futuremessenger/EditPreset.java
@@ -1,14 +1,18 @@
 package cs371m.hermes.futuremessenger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 
 public class EditPreset extends AppCompatActivity {
+
+    private final String TAG = "EditPreset";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,31 @@ public class EditPreset extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            fillEditTextFields(intent.getStringExtra("name"), intent.getStringExtra("message"));
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Should save message into Database", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //TODO: Save text into database
+                EditText name = (EditText) findViewById(R.id.edit_preset_name);
+                EditText message = (EditText) findViewById(R.id.edit_preset_message);
+                Log.d(TAG, name.getText().toString());
+                Log.d(TAG, message.getText().toString());
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void fillEditTextFields(String name_text, String msg_text) {
+        EditText name = (EditText) findViewById(R.id.edit_preset_name);
+        EditText message = (EditText) findViewById(R.id.edit_preset_message);
+        name.setText(name_text);
+        message.setText(msg_text);
     }
 
 }
--- app/src/main/java/cs371m/hermes/futuremessenger/ManagePresets.java
+++ app/src/main/java/cs371m/hermes/futuremessenger/ManagePresets.java
@@ -1,5 +1,6 @@
 package cs371m.hermes.futuremessenger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,11 +21,15 @@ public class ManagePresets extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                createNewPreset();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void createNewPreset() {
+        Intent intent = new Intent(this, EditPreset.class);
+        startActivityForResult(intent, 1);
     }
 
 }

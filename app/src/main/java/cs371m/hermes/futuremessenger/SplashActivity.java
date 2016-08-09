6a71fbe0b5e87ea5961b7a585b0a41f38ed7975e
Billy Do <dob@utexas.edu>
2016-08-09 17:35:10 -0500

    modified styles.xml to have background splash, making SplashActivity

---
+++ app/src/main/java/cs371m/hermes/futuremessenger/SplashActivity.java
@@ -0,0 +1,12 @@
+package cs371m.hermes.futuremessenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

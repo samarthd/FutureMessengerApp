02bf06dd4037ccae480a5294341fc090470646ec
Samarth Dhruva <samarthdhruva@utexas.edu>
2016-08-12 02:16:14 -0500

    Attempt to use combination of third party library code and Lollipop sendMultimediaMessage() method to send MMS programmatically. As of now, it's giving a MMS_ERROR_IO_ERROR (value of 5) result code on a real device (Samsung).  On the emulators the message fails after "No Service" result code, but it actually shows up in the default messaging app with the exact recipient, image, and body text!

--- app/src/main/java/cs371m/hermes/futuremessenger/AlarmReceiver.java
+++ app/src/main/java/cs371m/hermes/futuremessenger/AlarmReceiver.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -12,15 +14,50 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.provider.Telephony;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.telephony.SmsManager;
+import android.text.TextUtils;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
+
+import com.android.mms.dom.smil.parser.SmilXmlSerializer;
+import com.android.mms.service_alt.MmsConfig;
+import com.android.mms.service_alt.MmsConfigManager;
+import com.android.mms.service_alt.SendRequest;
+import com.google.android.mms.ContentType;
+import com.google.android.mms.InvalidHeaderValueException;
+import com.google.android.mms.MMSPart;
+import com.google.android.mms.MmsException;
+import com.google.android.mms.pdu_alt.CharacterSets;
+import com.google.android.mms.pdu_alt.EncodedStringValue;
+import com.google.android.mms.pdu_alt.PduBody;
+import com.google.android.mms.pdu_alt.PduComposer;
+import com.google.android.mms.pdu_alt.PduHeaders;
+import com.google.android.mms.pdu_alt.PduPart;
+import com.google.android.mms.pdu_alt.PduPersister;
+import com.google.android.mms.pdu_alt.SendReq;
+import com.google.android.mms.smil.SmilHelper;
+import com.klinker.android.send_message.Message;
+import com.klinker.android.send_message.MmsSentReceiver;
+import com.klinker.android.send_message.Transaction;
+import com.klinker.android.send_message.Utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Drew on 7/18/2016.
@@ -83,10 +120,24 @@ public class AlarmReceiver extends Service {
             int groupFlag = results.getInt("group_flag");
             String img_path = results.getString("image_path");
 
+            String[] indivNums = numbers.split(";");
+
             if(img_path != null){
                 Log.d(TAG, "onStart, " + img_path);
                 Log.d(TAG, "onStart, about to send picture MMS");
-                sendPictureMMS(messageID, numbers, messageText, img_path);
+                //sendPictureMMS(messageID, numbers, messageText, img_path);
+
+                try {
+                    Uri imageUri = Uri.parse(img_path);
+                    Bitmap image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    //Jpeg, png, ??
+                    String imageType = getContentResolver().getType(imageUri);
+                    handleMMS(messageText, indivNums, new Bitmap[] {image}, new String[] {imageType}, "", messageID);
+                }
+                catch (IOException e) {
+                    Log.d("About to send picMMS", "IO Exception");
+                }
+
             }
             else if (groupFlag == MessengerDatabaseHelper.IS_GROUP_MESSAGE){
                 Log.d(TAG, "onStart, about to send group MMS");
@@ -298,5 +349,226 @@ public class AlarmReceiver extends Service {
             ex.printStackTrace();
         }
     }
+
+
+    /* MMS Sending. Code taken from
+    https://github.com/klinker41/android-smsmms
+     */
+
+    /* This code is used to build and send an MMS message to a recipient.
+    *  Heavily based on Jacob Klinker's own implementation from https://github.com/klinker41
+     *  Made my own modifications to some of his internal methods to get it working in addition
+     *  to using some helpful classes that he made. Started with sendMmsMessage method in
+      *  Transaction.java*/
+    private void handleMMS (String messageText, String[] recipientNums, Bitmap[] images, String[] imgMimeTypes, String subject, final long messageID) {
+
+
+        // Data chunks for the actual MMS
+        ArrayList<MMSPart> data = new ArrayList<>();
+
+        // Get all images from the bitmap array and put them in the MMS
+        for (int i = 0; i < images.length; i++) {
+            // Convert bitmap to bytes for storage
+            byte[] imageBytes = Message.bitmapToByteArray(images[i]);
+            // Is this a jpeg, a png, ??
+            MMSPart curImagePart = new MMSPart();
+            /** TODO: Properly set the type based on what kind of image it is (the calling method should
+               automatically get this from the URI */
+            curImagePart.MimeType = imgMimeTypes[i];
+            curImagePart.Name = "image" + i;
+            curImagePart.Data = imageBytes;
+            data.add(curImagePart);
+        }
+
+        // Get the text part of the message if it exists
+        if (!messageText.equals("")) {
+            MMSPart textPart = new MMSPart();
+            textPart.Name = "text";
+            textPart.MimeType = "text/plain";
+            textPart.Data = messageText.getBytes();
+            data.add(textPart);
+        }
+
+        //build the pdu
+        SendReq curReq = buildPdu(this, recipientNums, subject, data);
+        processMMSSending(curReq, messageID, messageText);
+    }
+
+    /* Again, this code is heavily based on Jacob Klinker's own implementation in
+       https://github.com/klinker41. I have made small adjustments to fit my needs.
+     *  */
+    private void processMMSSending (SendReq curReq, final long messageID, final String messageText) {
+
+        final String fileName = "send." + String.valueOf(Math.abs(new Random().nextLong())) + ".dat";
+        File sendingFile = new File (this.getExternalFilesDir(null), fileName);
+        PduPersister persister = PduPersister.getPduPersister(this);
+        try {
+ /*          This method only works if you're the default SMS app.
+                Uri messageUri = persister.persist(curReq, Uri.parse("content://mms/outbox"),
+                    true, true, null);*/
+
+            // Build an intent for the sent receiver
+            String MMS_SENT_ACTION = "mms sent " + messageID;
+            Intent intent = new Intent(MMS_SENT_ACTION);
+            intent.putExtra("message", messageText);
+            final PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            //Write the data to the file
+            Uri writerUri = (new Uri.Builder())
+                   // .authority(this.getPackageName() + ".MmsFileProvider")
+                    .path(fileName)
+                    .scheme(ContentResolver.SCHEME_CONTENT)
+                    .build();
+            FileOutputStream writer = null;
+            Uri contentUri = null;
+            try {
+                writer = new FileOutputStream(sendingFile);
+                writer.write(new PduComposer(this, curReq).make());
+                contentUri = writerUri;
+            } catch (final IOException e) {
+                com.klinker.android.logger.Log.e(TAG, "Error writing send file", e);
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+
+            //Bundle configOverrides = new Bundle();
+            //configOverrides.putBoolean(SmsManager.MMS_CONFIG_GROUP_MMS_ENABLED, true);
+            if (contentUri != null) {
+                /* Register to listen for MMS send action in order to notify the user on send status */
+                registerReceiver(new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        String result = "";
+                        Resources curResources = getResources();
+                        switch (getResultCode()) {
+                            case Activity.RESULT_OK:
+                                result = curResources.getString(R.string.send_success);
+                                break;
+                            case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                                result = curResources.getString(R.string.send_fail);
+                                break;
+                            case SmsManager.RESULT_ERROR_RADIO_OFF:
+                                result = curResources.getString(R.string.radio_off);
+                                break;
+                            case SmsManager.RESULT_ERROR_NULL_PDU:
+                                result = curResources.getString(R.string.no_pdu);
+                                break;
+                            case SmsManager.RESULT_ERROR_NO_SERVICE:
+                                result = curResources.getString(R.string.no_service);
+                                break;
+                        }
+                        Log.d("MMSSentReceiver", "result: " + result + " resultCode: " + getResultCode());
+                        sendDeliveryNotification(messageID, result, messageText);
+                        unregisterReceiver(this);
+                    }
+                }, new IntentFilter(MMS_SENT_ACTION));
+
+
+                SmsManager.getDefault().sendMultimediaMessage(this,
+                        contentUri, null, null, pendingIntent);
+            } else {
+                com.klinker.android.logger.Log.e(TAG, "Error writing sending Mms");
+                try {
+                    pendingIntent.send(SmsManager.MMS_ERROR_IO_ERROR);
+                } catch (PendingIntent.CanceledException ex) {
+                    com.klinker.android.logger.Log.e(TAG, "Mms pending intent cancelled?", ex);
+                }
+            }
+
+
+        }
+        catch (Exception e) {
+            Log.e("Process MMS Sending", "Sending exception.", e);
+        }
+    }
+
+
+    /* Code copied and modified slightly from Jacob Klinker */
+    private static SendReq buildPdu(Context context, String[] recipients, String subject,
+                                    List<MMSPart> parts) {
+        final SendReq req = new SendReq();
+        // From, per spec
+        final String lineNumber = Utils.getMyPhoneNumber(context);
+        if (!TextUtils.isEmpty(lineNumber)) {
+            req.setFrom(new EncodedStringValue(lineNumber));
+        }
+        // To
+        for (String recipient : recipients) {
+            req.addTo(new EncodedStringValue(recipient));
+        }
+        // Subject
+        if (!TextUtils.isEmpty(subject)) {
+            req.setSubject(new EncodedStringValue(subject));
+        }
+        // Date
+        req.setDate(System.currentTimeMillis() / 1000);
+        // Body
+        PduBody body = new PduBody();
+        // Add text part. Always add a smil part for compatibility, without it there
+        // may be issues on some carriers/client apps
+        int size = 0;
+        for (int i = 0; i < parts.size(); i++) {
+            MMSPart part = parts.get(i);
+            size += addTextPart(body, part, i);
+        }
+
+        // add a SMIL document for compatibility
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        SmilXmlSerializer.serialize(SmilHelper.createSmilDocument(body), out);
+        PduPart smilPart = new PduPart();
+        smilPart.setContentId("smil".getBytes());
+        smilPart.setContentLocation("smil.xml".getBytes());
+        smilPart.setContentType(ContentType.APP_SMIL.getBytes());
+        smilPart.setData(out.toByteArray());
+        body.addPart(0, smilPart);
+
+        req.setBody(body);
+        // Message size
+        req.setMessageSize(size);
+        // Message class
+        req.setMessageClass(PduHeaders.MESSAGE_CLASS_PERSONAL_STR.getBytes());
+        // Expiry
+        req.setExpiry(Transaction.DEFAULT_EXPIRY_TIME);
+        try {
+            // Priority
+            req.setPriority(Transaction.DEFAULT_PRIORITY);
+            // Delivery report
+            req.setDeliveryReport(PduHeaders.VALUE_NO);
+            // Read report
+            req.setReadReport(PduHeaders.VALUE_NO);
+        } catch (InvalidHeaderValueException e) {}
+
+        return req;
+    }
+
+    // Entirely Jacob Klinker
+    private static int addTextPart(PduBody pb, MMSPart p, int id) {
+        String filename = p.MimeType.split("/")[0] + "_" + id + ".mms";
+        final PduPart part = new PduPart();
+        // Set Charset if it's a text media.
+        if (p.MimeType.startsWith("text")) {
+            part.setCharset(CharacterSets.UTF_8);
+        }
+        // Set Content-Type.
+        part.setContentType(p.MimeType.getBytes());
+        // Set Content-Location.
+        part.setContentLocation(filename.getBytes());
+        int index = filename.lastIndexOf(".");
+        String contentId = (index == -1) ? filename
+                : filename.substring(0, index);
+        part.setContentId(contentId.getBytes());
+        part.setData(p.Data);
+        pb.addPart(part);
+
+        return part.getData().length;
+    }
+
+
 
 }

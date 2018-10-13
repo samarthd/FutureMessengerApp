5a06be36576ae0083d989d1d90950f3f12af993e
Samarth Dhruva <>
2018-10-13 12:49:00 -0500

    Removed dummy data options from floating menu

--- app/src/main/java/cs371m/hermes/futuremessenger/ui/main/screens/activities/MainActivity.java
+++ app/src/main/java/cs371m/hermes/futuremessenger/ui/main/screens/activities/MainActivity.java
@@ -135,169 +135,16 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                         .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
                         .setLabelBackgroundColor(Color.WHITE)
                         .create());
-        mSpeedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.floating_action_menu_message_button, R.drawable.text_icon)
-                        .setFabBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabel("SCHEDULED")
-                        .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabelBackgroundColor(Color.WHITE)
-                        .create());
-        mSpeedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.floating_action_menu_message_button2, R.drawable.text_icon)
-                        .setFabBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabel("FAILED")
-                        .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabelBackgroundColor(Color.WHITE)
-                        .create());
-        mSpeedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.floating_action_menu_message_button3, R.drawable.text_icon)
-                        .setFabBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabel("SENT")
-                        .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabelBackgroundColor(Color.WHITE)
-                        .create());
-        mSpeedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.floating_action_menu_message_button4, R.drawable.text_icon)
-                        .setFabBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabel("DELETE SCHEDULED")
-                        .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabelBackgroundColor(Color.WHITE)
-                        .create());
-        mSpeedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.floating_action_menu_message_button5, R.drawable.text_icon)
-                        .setFabBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabel("DELETE FAILED")
-                        .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabelBackgroundColor(Color.WHITE)
-                        .create());
-        mSpeedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.floating_action_menu_message_button6, R.drawable.text_icon)
-                        .setFabBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabel("DELETE SENT")
-                        .setLabelColor(ContextCompat.getColor(this, R.color.colorPrimary))
-                        .setLabelBackgroundColor(Color.WHITE)
-                        .create());
         mSpeedDialView.setOnActionSelectedListener(actionItem -> {
             switch (actionItem.getId()) {
                 case R.id.schedule_new_message_floating_button:
                     Intent intent = new Intent(this, EditTextMessageActivity.class);
                     startActivity(intent);
                     return false;
-                case R.id.floating_action_menu_message_button:
-                    //TODO Delete
-                    new TempInsertTask(this).execute(Status.SCHEDULED);
-                    return false;
-                case R.id.floating_action_menu_message_button2:
-                    new TempInsertTask(this).execute(Status.FAILED);
-                    return false;
-                case R.id.floating_action_menu_message_button3:
-                    new TempInsertTask(this).execute(Status.SENT);
-                    return false;
-                case R.id.floating_action_menu_message_button4:
-                    //TODO Delete
-                    new TempDeleteTask(this).execute(Status.SCHEDULED);
-                    return false;
-                case R.id.floating_action_menu_message_button5:
-                    new TempDeleteTask(this).execute(Status.FAILED);
-                    return false;
-                case R.id.floating_action_menu_message_button6:
-                    new TempDeleteTask(this).execute(Status.SENT);
-                    return false;
                 default:
                     return false;
             }
         });
-    }
-
-    //TODO delete
-    private static class TempInsertTask extends AsyncTask<String, Void, Integer> {
-
-        private WeakReference<Activity> weakActivity;
-
-        public TempInsertTask(Activity activity) {
-            this.weakActivity = new WeakReference<>(activity);
-        }
-
-        @Override
-        protected Integer doInBackground(String... voids) {
-            insertManyMessages(10L, voids[0]);
-            return null;
-        }
-
-        //TODO delete this
-        private void insertManyMessages(Long j, String messageStatus) {
-            AppDatabase db = AppDatabase.getInstance(weakActivity.get());
-            MessageDao mDao = db.messageDao();
-            RecipientDao rDao = db.recipientDao();
-            MessageRecipientJoinDao mrjDao = db.messageRecipientJoinDao();
-            Runnable create =
-                    () -> {
-                        for (long i = 0; i < j; i++) {
-                            Message message = createMessageWithVal(i, messageStatus);
-                            Long messageId = mDao.createMessage(message);
-                            Recipient recipient = createRecipientWithVal(i);
-                            Long recipientId = rDao.createRecipient(recipient);
-                            MessageRecipientJoin join = new MessageRecipientJoin();
-                            join.setRecipientID(recipientId);
-                            join.setMessageID(messageId);
-                            mrjDao.insert(join);
-
-                            if (i % 2 == 0) {
-                                Recipient recipient2 = createRecipientWithVal(i * 100000000);
-                                Long recipientId2 = rDao.createRecipient(recipient2);
-                                MessageRecipientJoin join2 = new MessageRecipientJoin();
-                                join2.setRecipientID(recipientId2);
-                                join2.setMessageID(messageId);
-                                mrjDao.insert(join2);
-                            }
-                        }
-                    };
-            db.runInTransaction(create);
-        }
-
-        private Message createMessageWithVal(Long val, String messageStatus) {
-            Message message = new Message();
-            message.setTextContent("Text content " + val);
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(2009, Calendar.SEPTEMBER, 30, 12, 59); // longest possible date time
-            calendar.setTimeInMillis(calendar.getTimeInMillis() + val * 1000000000);
-            message.setScheduledDateTime(calendar);
-            cs371m.hermes.futuremessenger.persistence.entities.embedded.Status status = new cs371m.hermes.futuremessenger.persistence.entities.embedded.Status();
-            status.setCode(messageStatus);
-            message.setStatus(status);
-            return message;
-        }
-
-        private Recipient createRecipientWithVal(Long val) {
-            Recipient recipient = new Recipient();
-            recipient.setName("Recipient name " + val);
-            recipient.setPhoneNumber("Phone number " + val);
-            return recipient;
-        }
-    }
-
-    private static class TempDeleteTask extends AsyncTask<String, Void, Integer> {
-
-        private WeakReference<Activity> weakActivity;
-
-        public TempDeleteTask(Activity activity) {
-            this.weakActivity = new WeakReference<>(activity);
-        }
-
-        @Override
-        protected Integer doInBackground(String... voids) {
-            deleteManyMessages(10L, voids[0]);
-            return null;
-        }
-
-        //TODO delete this
-        private void deleteManyMessages(Long j, String messageStatus) {
-            AppDatabase db = AppDatabase.getInstance(weakActivity.get());
-            MessageDao mDao = db.messageDao();
-            RecipientDao rDao = db.recipientDao();
-            MessageRecipientJoinDao mrjDao = db.messageRecipientJoinDao();
-            mDao.deleteAllMessagesWithStatusCode(messageStatus);
-        }
     }
 
     private void setUpViewPager(ViewPager viewPager) {
